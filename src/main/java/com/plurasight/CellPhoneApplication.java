af76709ea5b6b3afada4242a6337f45e3a644c3d
Dzi-niska <dzhamarko@gmail.com>
2025-10-01 10:32:54 -0500

    Initial commit

---
+++ src/main/java/com/plurasight/CellPhoneApplication.java
@@ -0,0 +1,84 @@
+package com.pluralsight;
+
+public class CellPhoneApplication {
+    public static void main(String[] args) {
+        // CellPhone object
+        CellPhone myPhone = new CellPhone();
+
+        // values
+        myPhone.setSerialNumber(1234567);
+        myPhone.setModel("iPhone X");
+        myPhone.setCarrier("AT&T");
+        myPhone.setPhoneNumber("800-555-5555");
+        myPhone.setOwner("Dana Wyatt");
+
+        // print them
+        System.out.println("Serial: " + myPhone.getSerialNumber());
+        System.out.println("Model: " + myPhone.getModel());
+        System.out.println("Carrier: " + myPhone.getCarrier());
+        System.out.println("Phone: " + myPhone.getPhoneNumber());
+        System.out.println("Owner: " + myPhone.getOwner());
+    }
+}
+
+    // CellPhone class
+class CellPhone {
+    private int serialNumber;
+    private String model;
+    private String carrier;
+    private String phoneNumber;
+    private String owner;
+
+    // constructor
+    public CellPhone() {
+        this.serialNumber = 0;
+        this.model = "";
+        this.carrier = "";
+        this.phoneNumber = "";
+        this.owner = "";
+    }
+
+    // getters and setters
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+}
+
+
+
